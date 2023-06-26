04747053d9ccc6880d11df1b3f143175e22fa2ea
Tamara Calic <tamara.calic1004@gmail.com>
2023-06-26 23:58:45 +0200

    Napravljena login forma

--- myApp/src/app/login/login.page.spec.ts
+++ myApp/src/app/login/login.page.spec.ts
@@ -1,4 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import { RouterTestingModule } from '@angular/router/testing';
 import { LoginPage } from './login.page';
 
 describe('LoginPage', () => {
@@ -6,6 +8,11 @@ describe('LoginPage', () => {
   let fixture: ComponentFixture<LoginPage>;
 
   beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [LoginPage],
+      imports: [IonicModule.forRoot(), RouterTestingModule],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- myApp/src/app/login/login.page.ts
+++ myApp/src/app/login/login.page.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  email: string='';
+  password: string='';
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+ login() {
+    // Ovde možete dodati logiku za prijavu korisnika
+    // Na primer, možete koristiti AuthService za proveru korisničkih podataka i autentifikaciju
+    // ili poslati HTTP zahtev ka serveru za proveru i autentifikaciju korisnika
+
+    // Nakon uspešne prijave, možete preusmeriti korisnika na drugu stranicu
+    this.router.navigate(['/home']);
   }
-
 }
