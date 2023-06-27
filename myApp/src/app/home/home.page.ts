80099acc9463807e5d95aa7eee4ab087ba277fb1
Tamara Calic <tamara.calic1004@gmail.com>
2023-06-27 19:41:18 +0200

    Kreirana galerija slika - Mina

--- myApp/src/app/home/home.page.ts
+++ myApp/src/app/home/home.page.ts
@@ -6,7 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  galleryImages: { url: string; title: string }[] = [
+    {
+      url: 'assets/img/slika1.jpg',
+      title: 'Image 1',
+    },
+    {
+      url: 'assets/img/slika2.jpg',
+      title: 'Image 2',
+    },
+    {
+      url: 'assets/img/slika3.jpg',
+      title: 'Image 3',
+    },
+    {
+      url: 'assets/img/slika4.jpg', 
+      title: 'Image 4',
+    }, 
+    {
+      url: 'assets/img/slika5.jpg', 
+      title: 'Image 5'
+    }
+  ];
 
   constructor() {}
-
 }
