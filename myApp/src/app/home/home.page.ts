646564822e007971fabb8fc55516a6197e82d4d9
Tamara Calic <tamara.calic1004@gmail.com>
2023-06-27 21:07:04 +0200

    Dodato dugme za dodavanje slika, title i opisa

--- myApp/src/app/home/home.page.ts
+++ myApp/src/app/home/home.page.ts
@@ -6,28 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  galleryImages: { url: string; title: string }[] = [
+  galleryImages: { url: string; title: string; description: string }[] = [
     {
       url: 'assets/img/slika1.jpg',
       title: 'Image 1',
+      description: 'Ovo je prva slika',
     },
     {
       url: 'assets/img/slika2.jpg',
       title: 'Image 2',
+      description: 'Ovo je druga slika',
     },
     {
       url: 'assets/img/slika3.jpg',
       title: 'Image 3',
+      description: 'Ovo je treca slika',
     },
     {
-      url: 'assets/img/slika4.jpg', 
+      url: 'assets/img/slika4.jpg',
       title: 'Image 4',
-    }, 
+      description: 'Ovo je cetvrta slika',
+    },
     {
-      url: 'assets/img/slika5.jpg', 
-      title: 'Image 5'
-    }
+      url: 'assets/img/slika5.jpg',
+      title: 'Image 5',
+      description: 'Ovo je peta slika',
+    },
   ];
 
   constructor() {}
+
+  addPicture(url: string, title: string, description: string) {
+    this.galleryImages.push({ url, title, description });
+  }
+
+  handleFileInput(event: any) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      const url = reader.result as string;
+      const title = prompt('Enter the image title:');
+      const description = prompt('Enter the image description:');
+      if (title && description) {
+        this.addPicture(url, title, description);
+      }
+    };
+    reader.readAsDataURL(file);
+  }
 }
