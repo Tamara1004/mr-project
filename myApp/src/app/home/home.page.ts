1d9fe32a613fb61ad5af07bd88c9b0f0bec09239
Tamara Calic <tamara.calic1004@gmail.com>
2023-06-27 21:55:14 +0200

    Kreirano Update dugme i poravljeno cuvanje azuriranih podataka - Mina

--- myApp/src/app/home/home.page.ts
+++ myApp/src/app/home/home.page.ts
@@ -6,51 +6,82 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  galleryImages: { url: string; title: string; description: string }[] = [
-    {
-      url: 'assets/img/slika1.jpg',
-      title: 'Image 1',
-      description: 'Ovo je prva slika',
-    },
-    {
-      url: 'assets/img/slika2.jpg',
-      title: 'Image 2',
-      description: 'Ovo je druga slika',
-    },
-    {
-      url: 'assets/img/slika3.jpg',
-      title: 'Image 3',
-      description: 'Ovo je treca slika',
-    },
-    {
-      url: 'assets/img/slika4.jpg',
-      title: 'Image 4',
-      description: 'Ovo je cetvrta slika',
-    },
-    {
-      url: 'assets/img/slika5.jpg',
-      title: 'Image 5',
-      description: 'Ovo je peta slika',
-    },
-  ];
+  galleryImages: { url: string; title: string; description: string }[];
 
-  constructor() {}
+  constructor() {
+    const savedImages = localStorage.getItem('galleryImages');
+
+    if (savedImages) {
+      this.galleryImages = JSON.parse(savedImages);
+    } else {
+      this.galleryImages = [
+        {
+          url: 'assets/img/slika1.jpg',
+          title: 'Image 1',
+          description: 'Ovo je prva slika',
+        },
+        {
+          url: 'assets/img/slika2.jpg',
+          title: 'Image 2',
+          description: 'Ovo je druga slika',
+        },
+        {
+          url: 'assets/img/slika3.jpg',
+          title: 'Image 3',
+          description: 'Ovo je treca slika',
+        },
+        {
+          url: 'assets/img/slika4.jpg',
+          title: 'Image 4',
+          description: 'Ovo je cetvrta slika',
+        },
+        {
+          url: 'assets/img/slika5.jpg',
+          title: 'Image 5',
+          description: 'Ovo je peta slika',
+        },
+      ];
+    }
+  }
 
   addPicture(url: string, title: string, description: string) {
     this.galleryImages.push({ url, title, description });
+    this.updateLocalStorage();
+  }
+
+  updatePicture(index: number) {
+    const newTitle = prompt('Enter the new title:', this.galleryImages[index].title);
+    const newDescription = prompt('Enter the new description:', this.galleryImages[index].description);
+
+    if (newTitle !== null) {
+      this.galleryImages[index].title = newTitle;
+    }
+    if (newDescription !== null) {
+      this.galleryImages[index].description = newDescription;
+    }
+    this.updateLocalStorage();
+  }
+
+  updateLocalStorage() {
+    localStorage.setItem('galleryImages', JSON.stringify(this.galleryImages));
   }
 
   handleFileInput(event: any) {
     const file = event.target.files[0];
     const reader = new FileReader();
+
     reader.onload = () => {
       const url = reader.result as string;
       const title = prompt('Enter the image title:');
       const description = prompt('Enter the image description:');
-      if (title && description) {
+
+      if (url && title && description) {
         this.addPicture(url, title, description);
       }
     };
-    reader.readAsDataURL(file);
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
   }
 }
